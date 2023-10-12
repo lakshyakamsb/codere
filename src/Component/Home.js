ff2ee5f02b908ca32e8c9d6a447fe36ee4a61ce0
lakshyakamsb <kandpallakshya79@gmail.com>
2023-10-12 13:01:27 +0530

    first commit

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -8,33 +8,33 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" style={divStyle}>
-        <ol className="carousel-indicators">
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-          <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="/image/selider1.jpg" alt="Slide 1" />
-          </div>
-          <div className="carousel-item">
-            <img src="image/selider2.jpg" className="d-block w-100" alt="Slide 2" />
-          </div>
-          <div className="carousel-item">
-            <img src="image/selider3.jpg" className="d-block w-100" alt="Slide 3" />
-          </div>
-        </div>
-        <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </a>
-        <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
-          <span className="carousel-control-next-icon" ariahidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </a>
+    <div><div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel" style={divStyle}>
+    <ol className="carousel-indicators">
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active"></li>
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+      <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+    </ol>
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <img src={`${process.env.PUBLIC_URL}/image/selider1.jpg`} alt="Slide 1" />
       </div>
+      <div className="carousel-item">
+        <img src={`${process.env.PUBLIC_URL}/image/selider2.jpg`} className="d-block w-100" alt="Slide 2" />
+      </div>
+      <div className="carousel-item">
+        <img src={`${process.env.PUBLIC_URL}/image/selider3.jpg`} className="d-block w-100" alt="Slide 3" />
+      </div>
+    </div>
+    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </a>
+    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </a>
+  </div>
+  
       <About></About>
       <Service></Service>
       <div id="gallery" className="container bg-info">
